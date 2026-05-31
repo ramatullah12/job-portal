e2c706f00d2cf4de792ec280caed983698ad4235
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-05-31 22:22:39 +0700

    job categories section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import JobCategories from "./components/JobCategories";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <JobCategories />
     </>
   );
 }
