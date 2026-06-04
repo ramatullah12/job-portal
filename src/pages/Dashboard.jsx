b7dcbdbc8d2756912454323c46c3afb6b9b0a0fb
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-04 11:08:35 +0700

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -229,7 +229,6 @@ function Dashboard() {
               </Link>
 
             </div>
-
           </div>
           {/* Recent Saved Jobs */}
           <div className="bg-white dark:bg-slate-800 border border-gray-200 dark:border-slate-700 rounded-2xl shadow p-6 mt-8">
