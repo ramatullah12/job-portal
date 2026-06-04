0c71f6b6f32f70cfa161b6dc0165eaa53e4da534
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-04 11:10:37 +0700

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -193,7 +193,6 @@ function Dashboard() {
 
           {/* Quick Actions */}
           <div className="bg-white dark:bg-slate-800 border border-gray-200 dark:border-slate-700 rounded-2xl shadow p-6 mt-8">
-
             <h2 className="text-2xl font-semibold text-slate-900 dark:text-white mb-6">
               Quick Actions
             </h2>
