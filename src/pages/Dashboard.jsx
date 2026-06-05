77f49c108069e68eb9e2c71a167dd0e77c6493ec
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-05 09:44:52 +0700

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -166,7 +166,6 @@ function Dashboard() {
             </div>
 
             <div className="bg-white dark:bg-slate-800 border border-gray-200 dark:border-slate-700 rounded-2xl shadow p-6">
-
               <h2 className="text-2xl font-semibold text-slate-900 dark:text-white mb-4">
                 Resume Status
               </h2>
