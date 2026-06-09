8871ebc0aa03a781e45002d7f1fe99d60efc6a51
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-09 09:53:59 +0700

    Update JobDetail.jsx

--- src/pages/JobDetail.jsx
+++ src/pages/JobDetail.jsx
@@ -233,7 +233,6 @@ function JobDetail() {
                     {job.company_name}
                   </p>
                 </div>
-
                 <div className="bg-slate-100 dark:bg-slate-700 p-5 rounded-xl">
                   <p className="font-semibold text-slate-900 dark:text-white">
                     Location
