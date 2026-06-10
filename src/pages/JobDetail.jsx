6a9146d6a58c09181933586460a9eb37b8d73118
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-10 14:58:55 +0700

    Update JobDetail.jsx

--- src/pages/JobDetail.jsx
+++ src/pages/JobDetail.jsx
@@ -191,7 +191,6 @@ function JobDetail() {
                 ✅ You have already applied for this job.
               </div>
             )}
-
             {/* Description */}
             <div className="mt-12">
               <h2 className="text-3xl font-bold text-slate-900 dark:text-white mb-6">
