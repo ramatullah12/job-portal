485bce685a25a013e98f6e31748d59ed05042398
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-10 15:07:22 +0700

    Update JobDetail.jsx

--- src/pages/JobDetail.jsx
+++ src/pages/JobDetail.jsx
@@ -81,7 +81,6 @@ function JobDetail() {
       <Navbar />
 
       <main className="min-h-screen bg-slate-50 dark:bg-slate-900 transition-colors duration-300">
-
         <div className="max-w-6xl mx-auto px-6 py-12">
           <Link
             to="/jobs"
