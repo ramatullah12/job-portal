d571f3acf33f5c0aa24e2fd6e31fb8f865658309
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-12 15:15:39 +0700

    Update AppliedJobs.jsx

--- src/pages/AppliedJobs.jsx
+++ src/pages/AppliedJobs.jsx
@@ -109,7 +109,6 @@ function AppliedJobs() {
               <h2 className="text-3xl font-bold text-slate-900 dark:text-white">
                 No Applications Yet
               </h2>
-
               <p className="text-gray-500 dark:text-gray-400 mt-3">
                 Start applying for jobs and your application history will appear here.
               </p>
