8f6c47a305552ee5c2b6c610e09df3f54aef3882
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-13 21:22:46 +0700

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -215,7 +215,6 @@ function Profile() {
             <div className="bg-white dark:bg-slate-800 border border-gray-200 dark:border-slate-700 rounded-2xl shadow p-6">
 
               <div className="flex flex-col items-center">
-
                 {photo ? (
                   <img
                     src={photo}
