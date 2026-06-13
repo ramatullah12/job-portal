2a05ed39d9528511421451112510c85b4d187336
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-13 21:20:09 +0700

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -310,7 +310,6 @@ function Profile() {
                 </div>
 
               </div>
-
               <hr className="my-6 border-gray-300 dark:border-slate-700" />
               <h3 className="font-semibold text-slate-900 dark:text-white mb-3">
                 Resume
