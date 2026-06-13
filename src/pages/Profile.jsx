874f2a105f59b442014552af89ec7af352670e39
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-13 21:21:17 +0700

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -284,7 +284,6 @@ function Profile() {
                   </div>
 
                   <div className="w-full bg-gray-200 dark:bg-slate-700 rounded-full h-3">
-
                     <div
                       className="bg-blue-600 h-3 rounded-full"
                       style={{
