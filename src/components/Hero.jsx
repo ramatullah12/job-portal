e2838799e0b04ded09ff98112b2b38944727f270
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-14 18:37:19 +0700

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -94,7 +94,6 @@ function Hero() {
               <h3 className="text-3xl font-bold text-purple-600">
                 50K+
               </h3>
-
               <p className="text-gray-500 dark:text-gray-400">
                 Candidates
               </p>
