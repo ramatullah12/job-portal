fcce32e0dcf4e9397269a9d3eb60c06a66b5e844
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-16 09:28:14 +0700

    Update Statistics.jsx

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -40,7 +40,6 @@ function Statistics() {
             Trusted by thousands of companies and professionals worldwide
           </p>
         </div>
-
         {/* Stats Grid */}
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
           {stats.map((item) => (
