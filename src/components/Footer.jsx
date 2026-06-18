0c584da086402e9388f73df7f70f9c7c9c116e7a
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-18 13:49:44 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -65,7 +65,6 @@ function Footer() {
             </h3>
 
             <ul className="space-y-3 text-gray-600 dark:text-gray-400">
-
               <li>
                 <Link
                   to="/jobs"
