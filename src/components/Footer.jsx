60eb953f15c468b5eb795f8045e8e4f22352d0a1
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-18 07:51:16 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -138,7 +138,6 @@ function Footer() {
 
         {/* Bottom Footer */}
         <div className="border-t border-gray-300 dark:border-slate-800 mt-12 pt-6 flex flex-col md:flex-row justify-between items-center">
-
           <p className="text-gray-500 dark:text-gray-400 text-sm">
             © 2026 CareerHub. All rights reserved.
           </p>
