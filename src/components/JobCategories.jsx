a6dad1c6d2daea74c002baea497298ebf5672e6c
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-21 21:32:36 +0700

    Update JobCategories.jsx

--- src/components/JobCategories.jsx
+++ src/components/JobCategories.jsx
@@ -41,7 +41,6 @@ function JobCategories() {
 
         {/* Categories */}
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6">
-
           {categories.map((category) => (
             <div
               key={category.name}
