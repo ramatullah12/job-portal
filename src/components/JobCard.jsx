601bb958355b81defa5c71d41621bd3e6acb468c
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-21 21:29:10 +0700

    Update JobCard.jsx

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -41,7 +41,7 @@ function JobCard({ job }) {
         {job.job_types?.[0] || "Full Time"}
       </span>
 
-  
+
       <h3 className="text-xl font-bold mt-4 text-slate-900 dark:text-white">
         {job.title}
       </h3>
