f9932902990a3b9ef4442397584934ce1c3d9126
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-22 19:58:41 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -129,7 +129,6 @@ function Navbar() {
 
         {/* Right Menu */}
         <div className="flex items-center gap-3">
-
           <button
             onClick={toggleTheme}
             className="
