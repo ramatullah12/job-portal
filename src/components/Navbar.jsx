f11b913e00157421e5360073c85b189a83f2b370
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-23 17:56:07 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ function Navbar() {
   return (
     <nav className="bg-white dark:bg-slate-900 border-b dark:border-slate-700 shadow-sm sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
-        {/* Logo */}
+        
         <Link
           to="/"
           className="text-2xl font-bold text-blue-600"
