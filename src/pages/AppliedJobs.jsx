9c3bc962799c915ddd8dc5b6d1b9b5460c12715b
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-23 17:59:52 +0700

    Update AppliedJobs.jsx

--- src/pages/AppliedJobs.jsx
+++ src/pages/AppliedJobs.jsx
@@ -152,7 +152,6 @@ function AppliedJobs() {
                       </p>
 
                     </div>
-
                     <div className="flex flex-col gap-3">
                       <span
                         className={`px-4 py-2 rounded-full text-center font-medium ${
