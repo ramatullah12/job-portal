b9267b5c9f65d0e936bc5f1613525668a5ae26f9
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-23 18:01:49 +0700

    Update AppliedJobs.jsx

--- src/pages/AppliedJobs.jsx
+++ src/pages/AppliedJobs.jsx
@@ -100,7 +100,7 @@ function AppliedJobs() {
             </div>
           </div>
 
-      
+    
           {applications.length === 0 ? (
             <div className="bg-white dark:bg-slate-800 border border-gray-200 dark:border-slate-700 rounded-2xl shadow p-12 text-center">
               <div className="text-7xl mb-4">
