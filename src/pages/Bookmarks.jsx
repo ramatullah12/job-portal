d15e81ec9201b8f4ff74c48a439dd5c90d956746
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-26 08:30:12 +0700

    Update Bookmarks.jsx

--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -54,7 +54,6 @@ function Bookmarks() {
               <h2 className="text-2xl font-bold text-slate-900 dark:text-white">
                 No Saved Jobs Yet
               </h2>
-
               <p className="text-gray-500 dark:text-gray-400 mt-3">
                 Start saving jobs to keep track of opportunities.
               </p>
