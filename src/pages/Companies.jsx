9ae9f2e3cdaaa487be41d2c0db8ef195302e7c8a
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-30 21:30:35 +0700

    Update Companies.jsx

--- src/pages/Companies.jsx
+++ src/pages/Companies.jsx
@@ -238,7 +238,6 @@ function Companies() {
                   <h2 className="text-2xl font-bold text-slate-900 dark:text-white">
                     {company.name}
                   </h2>
-
                   <p className="text-gray-500 dark:text-gray-400 mt-2">
                     {company.industry}
                   </p>
